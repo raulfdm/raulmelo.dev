2228768e429cb51332a360121bd675ba0d9bb27d
Raul de Melo <melo.raulf@gmail.com>
2019-09-21 21:21:12 +0200

    chore: remove boilerplate component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
 
 const Home = () => (
   <div>
@@ -9,29 +8,27 @@ const Home = () => (
       <title>Home</title>
     </Head>
 
-    <Nav />
-
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
+    <div className="hero">
+      <h1 className="title">Welcome to Next.js!</h1>
+      <p className="description">
         To get started, edit <code>pages/index.js</code> and save to reload.
       </p>
 
-      <div className='row'>
-        <Link href='https://github.com/zeit/next.js#setup'>
-          <a className='card'>
+      <div className="row">
+        <Link href="https://github.com/zeit/next.js#setup">
+          <a className="card">
             <h3>Getting Started &rarr;</h3>
             <p>Learn more about Next.js on GitHub and in their examples.</p>
           </a>
         </Link>
-        <Link href='https://github.com/zeit/next.js/tree/master/examples'>
-          <a className='card'>
+        <Link href="https://github.com/zeit/next.js/tree/master/examples">
+          <a className="card">
             <h3>Examples &rarr;</h3>
             <p>Find other example boilerplates on the Next.js GitHub.</p>
           </a>
         </Link>
-        <Link href='https://github.com/zeit/next.js'>
-          <a className='card'>
+        <Link href="https://github.com/zeit/next.js">
+          <a className="card">
             <h3>Create Next App &rarr;</h3>
             <p>Was this tool helpful? Let us know how we can improve it!</p>
           </a>
@@ -86,6 +83,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
