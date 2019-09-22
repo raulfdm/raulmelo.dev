cde9250bb69d0891f46144b06bd260a7e6b2c59a
Raul de Melo <melo.raulf@gmail.com>
2019-09-22 10:39:09 +0200

    feat: Implement author presentation section and add styled icons

---
+++ src/components/AuthorPresentation/AuthorPresentation.js
@@ -0,0 +1,115 @@
+import React from 'react';
+
+import styled, { css } from 'styled-components';
+import mediaQuery from 'styled-media-query';
+// import { Github } from 'styled-icons/boxicons-logos/Github';
+import { Twitter, Linkedin, Github } from 'styled-icons/boxicons-logos';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+  justify-content: space-between;
+  ${mediaQuery.greaterThan('medium')(css`
+    flex-direction: row;
+  `)}
+`;
+
+const ImageWrapper = styled.div`
+  max-width: 8rem;
+  margin-bottom: 2rem;
+
+  ${mediaQuery.greaterThan('medium')(css`
+    max-width: 12.8rem;
+    margin-left: 4rem;
+    margin-bottom: 0;
+  `)}
+`;
+
+const Image = styled.img`
+  border-radius: 50%;
+  width: 100%;
+`;
+
+const AuthorDataWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const AuthorName = styled.h1`
+  letter-spacing: -0.93px;
+  font-size: 3.6rem;
+  font-family: ${({ theme }) => theme.font.contentSans};
+  font-weight: 600;
+`;
+
+const AuthorSynopsis = styled.p`
+  font-size: 1.8rem;
+  font-weight: 400;
+  font-family: ${({ theme }) => theme.font.contentSans};
+`;
+
+const SocialWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  padding-top: 2rem;
+`;
+
+const SocialLink = styled.a`
+  color: ${({ theme }) => theme.color.black.medium};
+  position: relative;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  margin-right: 1.6rem;
+  &:not(:last-child):after {
+    content: '·';
+    color: inherit;
+    position: absolute;
+    right: -0.8rem;
+  }
+
+  svg {
+    color: inherit;
+  }
+`;
+
+const AuthorPresentation = ({
+  profilePic,
+  name,
+  synopsis,
+  github,
+  linkedIn,
+  twitter,
+}) => {
+  return (
+    <Wrapper>
+      <AuthorDataWrapper>
+        <AuthorName>{name}</AuthorName>
+        <AuthorSynopsis>{synopsis}</AuthorSynopsis>
+        <SocialWrapper>
+          {github && (
+            <SocialLink href={github}>
+              <Github size={21} />
+            </SocialLink>
+          )}
+          {twitter && (
+            <SocialLink href={twitter}>
+              <Twitter size={21} />
+            </SocialLink>
+          )}
+          {linkedIn && (
+            <SocialLink href={linkedIn}>
+              <Linkedin size={21} />
+            </SocialLink>
+          )}
+        </SocialWrapper>
+      </AuthorDataWrapper>
+      <ImageWrapper>
+        <Image src={profilePic} />
+      </ImageWrapper>
+    </Wrapper>
+  );
+};
+
+export default AuthorPresentation;
---
+++ src/components/AuthorPresentation/AuthorPresentation.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AuthorPresentation from './AuthorPresentation';
+
+export default {
+  title: 'AuthorPresentation',
+};
+
+export const defaultCase = () => (
+  <AuthorPresentation
+    name="Raul de Melo"
+    profilePic="https://miro.medium.com/fit/c/256/256/1*6jtMoNvX_MHslzBLP4aM9g.jpeg"
+    synopsis="Developer, passionate about coding for fun, tech addicted and open-source lover ❤"
+    twitter="https://twitter.com/raul_fdm"
+    linkedin=""
+    github=""
+  />
+);
