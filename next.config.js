15cb8112945b6ba6ed1e9e32d54de122e20e1e99
Raul de Melo <melo.raulf@gmail.com>
2019-09-22 17:55:51 +0200

    feat: try to add netlify cms

--- next.config.js
+++ next.config.js
@@ -1,5 +1,30 @@
 const withCSS = require('@zeit/next-css');
+const withPlugins = require('next-compose-plugins');
 
-module.exports = withCSS({
-  /* config options here */
+const webpackConfig = config => {
+  config.module.rules.push({
+    test: /\.md$/,
+    use: 'frontmatter-markdown-loader',
+  });
+
+  return config;
+};
+
+module.exports = withPlugins([[withCSS, {}]], {
+  webpack: webpackConfig,
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/readme.md': { page: '/readme' },
+      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+    };
+  },
 });
+
+exports.pathMap;
