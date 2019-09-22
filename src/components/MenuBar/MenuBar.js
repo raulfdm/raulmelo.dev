3ffba1611222c62bdba5975cd105c6902422d808
Raul de Melo <melo.raulf@gmail.com>
2019-09-22 09:45:14 +0200

    feat(menu-bar): implement Logo (mocked) and fix style

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+
 import { Container } from '../ui';
+import InputSearch from '../InputSearch';
+import LogoSvg from '../LogoSvg';
 
 const MenuBarWrapper = styled.nav`
   box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.05);
@@ -9,17 +12,31 @@ const MenuBarWrapper = styled.nav`
   left: 0;
   right: 0;
   height: 6.5rem;
+  z-index: 500;
+  background-color: #fff;
 `;
 
 const CustomContainer = styled(Container)`
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  max-width: 100%;
 `;
 
 const MenuBar = () => {
   return (
     <MenuBarWrapper>
-      <CustomContainer>gege</CustomContainer>
+      <CustomContainer>
+        <div class="u-xs-hide js-metabarLogoLeft">
+          <a href="https://medium.com/" data-log-event="home">
+            <span class="svgIcon svgIcon--logoWordmark svgIcon--122x45px u-xs-show u-flex">
+              <LogoSvg />
+            </span>
+            {/* <span class="u-textScreenReader">Homepage</span> */}
+          </a>
+        </div>
+        <InputSearch />
+      </CustomContainer>
     </MenuBarWrapper>
   );
 };
