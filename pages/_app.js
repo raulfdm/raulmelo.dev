dea8769629c18c9c3ede25e4568a9b2ff8f86b00
Raul de Melo <melo.raulf@gmail.com>
2019-09-22 09:45:45 +0200

    feat: add menu bar to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import styled from 'styled-components';
 import App from 'next/app';
 
+import MenuBar from '../src/components/MenuBar';
 import { ThemeProvider } from '../src/config/theme';
-import { ContainerCss } from '../src/components/ui';
+import { Container } from '../src/components/ui';
 import '../src/config/global.css';
-
-const Layout = styled.div`
-  ${ContainerCss}
-`;
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider>
-        <Layout>
+        <Container>
+          <MenuBar />
           <Component {...pageProps} />
-        </Layout>
+        </Container>
       </ThemeProvider>
     );
   }
