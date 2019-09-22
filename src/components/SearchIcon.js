bcc350881a07f7ffc13aa88422f23dc356a9166e
Raul de Melo <melo.raulf@gmail.com>
2019-09-22 09:20:14 +0200

    feat: implmenet input search

---
+++ src/components/SearchIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearcIcon = props => (
+  <svg width={25} height={25} {...props}>
+    <path d="M20.07 18.93l-4.16-4.15a6 6 0 10-.88.88l4.15 4.16a.62.62 0 10.89-.89zM6.5 11a4.75 4.75 0 119.5 0 4.75 4.75 0 01-9.5 0z" />
+  </svg>
+);
+
+export default SearcIcon;
