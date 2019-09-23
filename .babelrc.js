6d68751221429a8a2eb3f1c5d21b60bd37d87cf5
Raul de Melo <melo.raulf@gmail.com>
2019-09-23 22:38:28 +0200

    chore: add more data to babelrc

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ['next/babel'],
-  plugins: [['babel-plugin-styled-components', { ssr: true }]],
+  plugins: [
+    [
+      'babel-plugin-styled-components',
+      { ssr: true, displayName: true, preprocess: false },
+    ],
+  ],
 };
