87750ff875e1f793f13a49ac1d9bb15761de5624
Raul de Melo <melo.raulf@gmail.com>
2019-10-08 06:13:10 +0200

    Implement event to generate dynamic pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,8 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
 const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require('path');
+
+const { postUrl } = require('./src/utils/urls');
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
@@ -18,4 +20,61 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       value,
     });
   }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const blogPost = path.resolve('./src/templates/blog-post.js');
+  const result = await graphql(`
+    query {
+      allMarkdownRemark(
+        sort: { fields: [frontmatter___date], order: DESC }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            html
+            timeToRead
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              date(formatString: "MMMM DD, YYYY")
+              featuredImage {
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    throw result.errors;
+  }
+
+  // Create blog posts pages.
+  const posts = result.data.allMarkdownRemark.edges;
+
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+    const next = index === 0 ? null : posts[index - 1].node;
+
+    console.log('post', post);
+
+    createPage({
+      path: postUrl(post.node.fields.slug),
+      component: blogPost,
+      context: {
+        html: post.node.html,
+        slug: postUrl(post.node.fields.slug),
+        previous,
+        next,
+        ...post.node.frontmatter
+      },
+    });
+  });
 };
