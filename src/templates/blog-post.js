5b76c3dc03001cc01336f0bfc9f12ed4d159aceb
Raul de Melo <melo.raulf@gmail.com>
2019-11-02 19:12:23 +0100

    Adjust post styles

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,7 +7,8 @@ const global = css`
   body {
     line-height: 1.58;
     font-family: ${({ theme }) => theme.font.contentSerif};
-    font-size: 1.8rem;
+    font-size: 2.1rem;
+    word-break: break-word;
   }
   h1 {
     font-size: 4rem;
@@ -15,12 +16,14 @@ const global = css`
     line-height: 4.8rem;
   }
   p {
-    letter-spacing: -0.072px;
+    letter-spacing: -0.084px;
     margin-bottom: -8.28px;
-    margin-top: 2.808rem;
+    margin-top: 42px;
     font-style: normal;
     font-weight: 400;
+    text-rendering: optimizelegibility;
   }
+
   article {
     a {
       color: inherit;
@@ -41,6 +44,34 @@ const global = css`
     p {
       font-style: italic;
     }
+  }
+
+  hr {
+    border-style: none;
+    font-size: 28px;
+    font-weight: 300;
+    margin-top: 30px;
+    text-align: center;
+  }
+
+  hr::before {
+    content: '...';
+    letter-spacing: 0.6em;
+    text-indent: 0.6em;
+    line-height: 1.4;
+  }
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: ${({ theme }) => theme.font.contentSans};
+  }
+
+  h2 {
+    font-size: 3.4rem;
+    font-weight: 600;
   }
 `;
 
