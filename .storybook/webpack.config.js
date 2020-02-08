8cdfe1e24d43592404ea4027ab971957366a58e5
Raul <melo.raulf@gmail.com>
2020-02-08 19:25:46 +0100

    simplify storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,10 @@
 module.exports = ({ config }) => {
-  config.resolve.mainFields = ['browser', 'module', 'main'];
-
   config.module.rules.push({
-    test: /\.(ts|tsx|js)$/,
+    test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
     options: {
       presets: [['react-app', { flow: false, typescript: true }]],
-      plugins: [
-        require.resolve('@babel/plugin-proposal-class-properties'),
-        // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
-        require.resolve('babel-plugin-remove-graphql-queries'),
-      ],
     },
-    exclude: [/node_modules/],
   });
 
   config.resolve.extensions.push('.ts', '.tsx');
