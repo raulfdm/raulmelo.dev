17b2f86dfef16d80943dfbf041e9869d131d38db
Raul <melo.raulf@gmail.com>
2020-02-08 19:28:20 +0100

    remove proptypes

--- src/html.tsx
+++ src/html.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 export default function HTML(props) {
   return (
@@ -60,12 +59,3 @@ export default function HTML(props) {
     </html>
   );
 }
-
-HTML.propTypes = {
-  htmlAttributes: PropTypes.object,
-  headComponents: PropTypes.array,
-  bodyAttributes: PropTypes.object,
-  preBodyComponents: PropTypes.array,
-  body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
-};
