de3424f675a98adeb7662c7c6fb25935a64da2dd
Raul <melo.raulf@gmail.com>
2020-02-09 11:25:12 +0100

    split eslint rules for ts and js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,36 @@
 module.exports = {
-  extends: ['plugin:react/recommended', '@sub-tv/eslint-config'],
-  plugins: ['react-hooks'],
   settings: {
     react: {
       version: 'detect',
     },
   },
-  rules: {
-    '@typescript-eslint/ban-ts-ignore': 'warn',
-    '@typescript-eslint/explicit-function-return-type': [
-      'warn',
-      {
-        allowExpressions: true,
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['plugin:react/recommended', '@sub-tv/eslint-config'],
+      plugins: ['react-hooks'],
+      rules: {
+        'react/prop-types': 0,
+        '@typescript-eslint/ban-ts-ignore': 'warn',
+        '@typescript-eslint/explicit-function-return-type': [
+          'warn',
+          {
+            allowExpressions: true,
+          },
+        ],
       },
-    ],
-  },
+    },
+    {
+      files: ['*.js'],
+      extends: ['eslint:recommended', 'plugin:react/recommended'],
+      parser: 'babel-eslint',
+      env: {
+        node: true,
+        browser: true,
+      },
+      rules: {
+        'react/prop-types': 'warn',
+      },
+    },
+  ],
 };
