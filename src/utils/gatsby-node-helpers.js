09a05c0363d527a129dc529011764635d7caf884
Raul <melo.raulf@gmail.com>
2020-02-09 10:10:03 +0100

    ignore eslint for specific files

--- src/utils/gatsby-node-helpers.js
+++ src/utils/gatsby-node-helpers.js
@@ -1,18 +1,18 @@
+/* eslint-disable */
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
 const localizedSlug = ({ isDefault, locale, slug }) =>
   isDefault ? `/${slug}` : `/${locale}/${slug}`;
 
 // Use a little helper function to remove trailing slashes from paths
-exports.removeTrailingSlash = path =>
+exports.removeTrailingSlash = (path) =>
   path === `/` ? path : path.replace(/\/$/, ``);
 
-  
-  exports.generateSlug = ({ node, getNode, lang }) => {
-    const { default: isDefault } = lang;
-    const filePath = createFilePath({ node, getNode }).replace(/index.*/, '');
-    
-    return isDefault ? filePath : `${lang.path}${filePath}`;
-  };
-  
-  exports.localizedSlug = localizedSlug;
+exports.generateSlug = ({ node, getNode, lang }) => {
+  const { default: isDefault } = lang;
+  const filePath = createFilePath({ node, getNode }).replace(/index.*/, '');
+
+  return isDefault ? filePath : `${lang.path}${filePath}`;
+};
+
+exports.localizedSlug = localizedSlug;
