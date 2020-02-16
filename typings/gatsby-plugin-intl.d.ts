30367f15220a672cae1e60b4c9e8e5d894ced61d
Raul Melo <melo.raulf@gmail.com>
2020-02-16 13:31:41 +0100

    add types for gatsby plugin intl

---
+++ typings/gatsby-plugin-intl.d.ts
@@ -0,0 +1,10 @@
+declare module 'gatsby-plugin-intl' {
+  import * as gatsby from 'gatsby';
+  import React from 'react';
+
+  export * from 'react-intl';
+
+  export class Link<TState> extends gatsby.Link<TState> {}
+  export const navigate: typeof gatsby.navigate;
+  export const changeLocale: (language: string, to?: string) => void;
+}
