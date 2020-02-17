e1b0d750c36a7c8b5c289d5c15968f4c1b1a416d
Raul Melo <melo.raulf@gmail.com>
2020-02-17 09:06:12 +0100

    start implementation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,59 @@
+require('dotenv').config();
+
+const myQuery = `{
+  posts: allMarkdownRemark(
+    sort: { fields: [frontmatter___date], order: DESC }
+    limit: 1000
+  ) {
+    edges {
+      node {
+        objectID: id
+        excerpt(pruneLength: 5000)
+        fields {
+          slug
+          localizedSlug
+        }
+        frontmatter {
+          date
+          title
+          description
+          image {
+            publicURL
+          }
+        }
+      }
+    }
+  }
+}`;
+
+const flattenData = (data) =>
+  data.map(({ node: { frontmatter, ...rest } }) => {
+    return {
+      ...frontmatter,
+      date_timestamp: (new Date(frontmatter.date).getTime() / 1000).toFixed(0),
+      ...rest,
+    };
+  });
+
+const queries = [
+  {
+    query: myQuery,
+    transformer: ({ data }) => flattenData(data.posts.edges),
+    indexName: 'posts', // overrides main index name, optional
+    settings: {
+      // optional, any index settings
+      attributesToSnippet: ['excerpt:20'],
+    },
+  },
+];
+
 module.exports = {
+  /* TODO: Fix that */
+  siteMetadata: {
+    title: `Gatsby`,
+    siteUrl: `https://www.gatsbyjs.org`,
+    description: `Blazing fast modern site generator for React`,
+  },
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
@@ -66,5 +121,17 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000,
+        enablePartialUpdates: true,
+        matchFields: ['slug', 'modified'],
+      },
+    },
   ],
 };
