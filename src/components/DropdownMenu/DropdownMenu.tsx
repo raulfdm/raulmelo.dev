f599fdb1149134efc1b3e0cf35306289b90a5a77
Raul Melo <melo.raulf@gmail.com>
2020-02-23 11:56:08 +0100

    refactor: dropdown menu
    - move styles to a styled file
    - create types file
    - export styled components instead custom abstraction

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,73 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
+
+import * as S from './styled';
+import { DropdownMenuProps } from './types';
 import { ClickOutside } from '../ClickOutside';
 
-const Wrapper = styled.div`
-  color: ${(props) => props.theme.color.font};
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const StyledDropdownMenu = styled.div`
-  position: relative;
-`;
-
-const List = styled.ul`
-  background-color: ${(props) => props.theme.color.background};
-  border-radius: 3px;
-  box-shadow: ${(props) => props.theme.color.shadowLight} 0 1px 2px,
-    ${(props) => props.theme.color.shadow} 0 0 1px;
-  max-width: 280px;
-
-  margin: 0;
-  padding: 0;
-  position: absolute;
-  top: 20px;
-  right: 0;
-
-  display: flex;
-  flex-direction: column;
-`;
-
-const ArrowUp = styled.div`
-  clip: rect(0, 18px, 14px, -4px);
-  left: 50%;
-
-  margin-left: -7px;
-  position: absolute;
-  bottom: -8px;
-
-  &::after {
-    content: '';
-    display: block;
-    width: 14px;
-    height: 14px;
-    background: ${(props) => props.theme.color.background};
-    box-shadow: -1px -1px 1px -1px ${(props) => props.theme.color.shadow};
-    transform: rotate(45deg) translate(6px, 6px);
-  }
-`;
-
-type ToggleDropdown = () => void;
-
-type RenderPropsChildren = (args: {
-  isVisible: boolean;
-  toggleDropdown: ToggleDropdown;
-}) =>
-  | (Element & React.ReactHTMLElement<HTMLElement>)
-  | React.ReactElement<HTMLElement>;
-
-type Props = {
-  items: React.ReactNode;
-  children: RenderPropsChildren;
-};
-
-export const DropdownMenu: React.FC<Props> = ({ items, children }) => {
+export const DropdownMenu: React.FC<DropdownMenuProps> = ({
+  items,
+  children,
+}) => {
   const [isVisible, setIsVisible] = React.useState(false);
 
   return (
-    <Wrapper>
+    <S.DropdownMenuWrapper>
       <ClickOutside handleClickOutside={() => setIsVisible(false)}>
         {children({
           isVisible,
@@ -79,12 +23,12 @@ export const DropdownMenu: React.FC<Props> = ({ items, children }) => {
 
       {isVisible && (
         <>
-          <StyledDropdownMenu>
-            <List>{items}</List>
-          </StyledDropdownMenu>
-          <ArrowUp />
+          <S.DropdownMenu>
+            <S.DropdownMenuList>{items}</S.DropdownMenuList>
+          </S.DropdownMenu>
+          <S.ArrowUp />
         </>
       )}
-    </Wrapper>
+    </S.DropdownMenuWrapper>
   );
 };
