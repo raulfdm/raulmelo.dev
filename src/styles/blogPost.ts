ce3ea4a1a4ec2d5ddbed2df85bb82433de864388
Raul Melo <melo.raulf@gmail.com>
2020-02-23 12:00:52 +0100

    refactor: move blog post styles to styles folder

---
+++ src/styles/blogPost.ts
@@ -0,0 +1,77 @@
+import { css, createGlobalStyle } from 'styled-components';
+
+const global = css`
+  body {
+    line-height: 1.58;
+    font-family: ${({ theme }) => theme.font.contentSerif};
+    font-size: 2.1rem;
+    word-break: break-word;
+  }
+  h1 {
+    font-size: 4rem;
+    font-family: ${({ theme }) => theme.font.contentTitle};
+    line-height: 4.8rem;
+  }
+  p {
+    letter-spacing: -0.084px;
+    margin-bottom: -8.28px;
+    margin-top: 42px;
+    font-style: normal;
+    font-weight: 400;
+    text-rendering: optimizelegibility;
+  }
+
+  article {
+    a {
+      color: inherit;
+      background-image: linear-gradient(
+        to right,
+        rgba(0, 0, 0, 0.84) 100%,
+        rgba(0, 0, 0, 0.84) 0
+      );
+      background-size: 1px 1px;
+      background-position: 0 calc(1em + 1px);
+      background-repeat: repeat-x;
+    }
+  }
+  blockquote {
+    margin-left: -2rem;
+    padding-left: 2.3rem;
+    box-shadow: inset 3px 0 0 0 rgba(0, 0, 0, 0.84);
+    p {
+      font-style: italic;
+    }
+  }
+
+  hr {
+    border-style: none;
+    font-size: 28px;
+    font-weight: 300;
+    margin-top: 30px;
+    text-align: center;
+  }
+
+  hr::before {
+    content: '...';
+    letter-spacing: 0.6em;
+    text-indent: 0.6em;
+    line-height: 1.4;
+  }
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: ${({ theme }) => theme.font.contentSans};
+  }
+
+  h2 {
+    font-size: 3.4rem;
+    font-weight: 600;
+  }
+`;
+
+export const BlogGlobalStyle = createGlobalStyle`
+  ${global}
+`;
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { css, createGlobalStyle } from 'styled-components';
 
 import Layout from '../components/Layout';
+import { BlogGlobalStyle } from '../styles/blogPost';
 
 type PostProps = {
   pageContext: {
@@ -19,83 +19,6 @@ type PostProps = {
   };
 };
 
-/* TODO: Extract into a styled file */
-const global = css`
-  body {
-    line-height: 1.58;
-    font-family: ${({ theme }) => theme.font.contentSerif};
-    font-size: 2.1rem;
-    word-break: break-word;
-  }
-  h1 {
-    font-size: 4rem;
-    font-family: ${({ theme }) => theme.font.contentTitle};
-    line-height: 4.8rem;
-  }
-  p {
-    letter-spacing: -0.084px;
-    margin-bottom: -8.28px;
-    margin-top: 42px;
-    font-style: normal;
-    font-weight: 400;
-    text-rendering: optimizelegibility;
-  }
-
-  article {
-    a {
-      color: inherit;
-      background-image: linear-gradient(
-        to right,
-        rgba(0, 0, 0, 0.84) 100%,
-        rgba(0, 0, 0, 0.84) 0
-      );
-      background-size: 1px 1px;
-      background-position: 0 calc(1em + 1px);
-      background-repeat: repeat-x;
-    }
-  }
-  blockquote {
-    margin-left: -2rem;
-    padding-left: 2.3rem;
-    box-shadow: inset 3px 0 0 0 rgba(0, 0, 0, 0.84);
-    p {
-      font-style: italic;
-    }
-  }
-
-  hr {
-    border-style: none;
-    font-size: 28px;
-    font-weight: 300;
-    margin-top: 30px;
-    text-align: center;
-  }
-
-  hr::before {
-    content: '...';
-    letter-spacing: 0.6em;
-    text-indent: 0.6em;
-    line-height: 1.4;
-  }
-
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-family: ${({ theme }) => theme.font.contentSans};
-  }
-
-  h2 {
-    font-size: 3.4rem;
-    font-weight: 600;
-  }
-`;
-
-const BlogStyle = createGlobalStyle`
-  ${global}
-`;
-
 const Post: React.FC<PostProps> = ({ pageContext }) => {
   const { postByLocale, intl } = pageContext;
 
@@ -104,7 +27,7 @@ const Post: React.FC<PostProps> = ({ pageContext }) => {
   if (!post) {
     return (
       <Layout>
-        <BlogStyle />
+        <BlogGlobalStyle />
         <h1>not found</h1>
       </Layout>
     );
@@ -114,7 +37,7 @@ const Post: React.FC<PostProps> = ({ pageContext }) => {
 
   return (
     <Layout>
-      <BlogStyle />
+      <BlogGlobalStyle />
       <h1>{title}</h1>
 
       <article dangerouslySetInnerHTML={{ __html: html }} />
