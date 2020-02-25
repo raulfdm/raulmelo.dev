6ba97b27609b96aca8c04d4222eb6580adc7d488
Raul <melo.raulf@gmail.com>
2020-02-25 09:55:16 +0100

    fix: implement proxy to algolia search
    - Now it won't load anything until you type something;
    - Also it'll debounce the request for 500ms

--- src/types/Algolia.ts
+++ src/types/Algolia.ts
@@ -10,3 +10,16 @@ export type HitAlgolia = {
   timeToRead: number;
   date: string;
 };
+
+export type RequestAlgoliaClient = {
+  indexName: string;
+  params: {
+    highlightPreTag: string;
+    highlightPostTag: string;
+    query: string;
+    facets: string[];
+    tagFilters: string;
+  };
+};
+
+export type RequestsAlgoliaClient = RequestAlgoliaClient[];
