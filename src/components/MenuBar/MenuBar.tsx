5f4c353988fe77e9e196f48e3f85f713cc52d4d7
Raul Melo <melo.raulf@gmail.com>
2020-02-25 22:49:17 +0100

    feat(ui): add animation hide/show menu bar

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import { useScroll } from 'react-use-gesture';
+import { useSpring } from 'react-spring';
 import { Search as SearchIcon } from 'styled-icons/evil/Search';
 
 import { LanguageSwitch } from '../LanguageSwitch';
@@ -8,9 +10,42 @@ import LogoSvg from '../LogoSvg';
 import * as S from './styled';
 
 export const MenuBar: React.FC = () => {
+  const [showMenu, setShowMenu] = React.useState(true);
+
+  const bind = useScroll(
+    ({ direction: [_, y] }) => {
+      /* -1 means scrolling up  */
+      const nextShowMenuState = y === -1;
+
+      /* Avoid multiple useless set state */
+      if (nextShowMenuState === showMenu) return;
+
+      setShowMenu(nextShowMenuState);
+    },
+    /* This validation is required to bypass gatsby build.
+    However client-side will works fine */
+    { domTarget: typeof window !== 'undefined' ? window : undefined },
+  );
+
+  /* Not using First class saves TS warnings about the returning type
+  for the cases "window" isn't defined (aka building time)
+  */
+  React.useEffect(() => {
+    bind();
+  }, [bind]);
+
+  const props = useSpring({
+    transform: `translateY(${showMenu ? 0 : -100}px)`,
+    config: {
+      mass: 1,
+      tension: 300,
+      friction: 40,
+    },
+  });
+
   return (
     <React.Fragment>
-      <S.MenuBarWrapper>
+      <S.MenuBarWrapper style={props}>
         <S.CustomContainer>
           <S.LogoWrapper>
             <Link to="/">
