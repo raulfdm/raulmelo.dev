4f11acfb1e1c327bca16901b619f8cc290855c14
Raul de Melo <melo.raulf@gmail.com>
2020-05-23 11:09:12 +0200

    fix: remove gatsby offline pluging and pin dependencies (#577)

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,9 +48,7 @@ module.exports = {
         name: 'data',
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
     {
       resolve: 'gatsby-plugin-tinacms',
       options: {
