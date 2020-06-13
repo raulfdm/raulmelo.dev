ac81c54a94f7d089f56eb9f590c3991a2add4796
Raul Melo <melo.raulf@gmail.com>
2020-06-13 20:06:26 +0200

    feat: create a barrel to expose everything from emotion
    This helps to keep the same API as styled component and avoid imports overload

---
+++ src/styles/emotion.ts
@@ -0,0 +1,25 @@
+import defaultStyled, { CreateStyled } from '@emotion/styled';
+
+export interface SiteTheme {
+  isDarkTheme: boolean;
+  font: {
+    contentSans: string;
+    contentSerif: string;
+    contentTitle: string;
+  };
+  color: {
+    background: string;
+    font: string;
+    fontMedium: string;
+    fontLight: string;
+    border: string;
+    shadow: string;
+    shadowLight: string;
+    shadowBright: string;
+    shadowMenu: string;
+  };
+}
+
+export const styled = defaultStyled as CreateStyled<SiteTheme>;
+export { css, Global } from '@emotion/core';
+export { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
