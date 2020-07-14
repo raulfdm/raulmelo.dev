eb2b941da5177c64aa7b4f08a4a092729aacefc7
Raul Melo <melo.raulf@gmail.com>
2020-07-14 22:21:44 +0200

    refactor: extract useScrollToTop functionality to a custom hook

---
+++ src/hooks/useScrollToTop.tsx
@@ -0,0 +1,13 @@
+import { polyfill as scrollPolyfill } from 'smoothscroll-polyfill';
+
+if (typeof window !== `undefined`) {
+  scrollPolyfill();
+}
+
+const moveToTop = (): void => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
+export function useScrollToTop() {
+  return { moveToTop };
+}
