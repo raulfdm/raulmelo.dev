48b9a34fd3f11f82b484d8eb39da3353953c4a5c
Raul Melo <melo.raulf@gmail.com>
2020-07-14 22:40:11 +0200

    refactor: remove tinaCMS until production bug gets fixed

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { useLocalJsonForm } from 'gatsby-tinacms-json';
 import { graphql } from 'gatsby';
 import { ThemeProvider } from 'emotion-theming';
 import 'sanitize.css/sanitize.css';
 import { ArrowheadUp } from '@styled-icons/evaicons-solid/ArrowheadUp';
-import { polyfill as scrollPolyfill } from 'smoothscroll-polyfill';
 
 import {
   Info,
-  FormConfig,
   CareerSummary,
   CareerExperience,
   TechnicalSkills,
@@ -16,40 +13,21 @@ import {
   Education,
   SideProjects,
 } from 'components/CV';
-import { JsonNode } from 'types';
-import { CvJsonEdge, CvJson } from 'graphql-types';
+import { CvJson } from 'graphql-types';
 import { theme, GlobalCVStyles } from 'components/CV/styles';
 import { CVMain, HomeLink, ScrollToTopButton } from 'components/CV/UI';
-import { useIntl } from 'context/react-intl';
 import SEO from 'components/SEO';
 import { titleWithNameAndJobTitle } from 'utils/seo';
-
-if (typeof window !== `undefined`) {
-  scrollPolyfill();
-}
+import { useScrollToTop } from 'hooks/useScrollToTop';
 
 type CvPageProps = {
   data: {
-    allCvJson: {
-      edges: CvJsonEdge[];
-    };
+    cvJson: CvJson;
   };
 };
 
-const moveToTop = (): void => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-};
-
-const CvPage: React.FC<CvPageProps> = ({ data: apiData }) => {
-  const { locale } = useIntl();
-  const dataByLocale = apiData.allCvJson.edges.find(
-    (e) => e.node.lang === locale,
-  )!;
-
-  const [data]: [CvJson, unknown] = useLocalJsonForm(
-    (dataByLocale.node as unknown) as JsonNode,
-    FormConfig,
-  );
+const CvPage: React.FC<CvPageProps> = ({ data: { cvJson } }) => {
+  const { moveToTop } = useScrollToTop();
 
   return (
     <>
@@ -68,13 +46,13 @@ const CvPage: React.FC<CvPageProps> = ({ data: apiData }) => {
       <HomeLink to="/">Back to home</HomeLink>
       <CVMain>
         <ThemeProvider theme={theme}>
-          <Info data={data.info!} />
-          <CareerSummary data={data.career_summary!} />
-          <TechnicalSkills data={data.technical_skills!} />
-          <CareerExperience data={data.career_history!} />
-          <SideProjects data={data.side_projects!} />
-          <Education data={data.education!} />
-          <Interests data={data.interests!} />
+          <Info data={cvJson.info!} />
+          <CareerSummary data={cvJson.career_summary!} />
+          <TechnicalSkills data={cvJson.technical_skills!} />
+          <CareerExperience data={cvJson.career_history!} />
+          <SideProjects data={cvJson.side_projects!} />
+          <Education data={cvJson.education!} />
+          <Interests data={cvJson.interests!} />
         </ThemeProvider>
         <ScrollToTopButton onClick={moveToTop}>
           <ArrowheadUp size={21} />
@@ -86,93 +64,86 @@ const CvPage: React.FC<CvPageProps> = ({ data: apiData }) => {
 
 export const pageQuery = graphql`
   query DataQuery {
-    allCvJson(filter: { path: { eq: "/cv" } }) {
-      edges {
-        node {
-          lang
-          fileRelativePath
-          rawJson
-          info {
+    cvJson {
+      info {
+        name
+        phone
+        city
+        email
+        linkedIn
+        github
+      }
+      side_projects {
+        section_title
+        projects {
+          id
+          name
+          start_date
+          end_date
+          description
+          is_ongoing
+          show
+        }
+      }
+      career_summary {
+        section_title
+        description
+      }
+      technical_skills {
+        section_title
+        skills {
+          id
+          technologies {
+            id
             name
-            phone
-            city
-            email
-            linkedIn
-            github
           }
-          side_projects {
-            section_title
-            projects {
+          group_name
+        }
+      }
+      education {
+        section_title
+        formal {
+          id
+          foundation
+          start_date
+          end_date
+          title
+        }
+        languages {
+          id
+          name
+          proficiency
+        }
+        extra_courses {
+          section_title
+          platforms {
+            id
+            name
+            courses {
               id
               name
-              start_date
-              end_date
-              description
-              is_ongoing
-              show
+              hours
             }
           }
-          career_summary {
-            section_title
-            description
-          }
-          technical_skills {
-            section_title
-            skills {
-              id
-              technologies {
-                id
-                name
-              }
-              group_name
-            }
-          }
-          education {
-            section_title
-            formal {
-              id
-              foundation
-              start_date
-              end_date
-              title
-            }
-            languages {
-              id
-              name
-              proficiency
-            }
-            extra_courses {
-              section_title
-              platforms {
-                id
-                name
-                courses {
-                  id
-                  name
-                  hours
-                }
-              }
-            }
-          }
-          career_history {
-            section_title
-            jobs {
-              id
-              role
-              description
-              company
-              end_date
-              is_actual
-              start_date
-            }
-          }
-          interests {
-            section_title
-            values {
-              id
-              name
-            }
-          }
+        }
+      }
+      career_history {
+        section_title
+        jobs {
+          id
+          role
+          description
+          company
+          end_date
+          is_actual
+          start_date
+        }
+      }
+      interests {
+        section_title
+        values {
+          id
+          name
         }
       }
     }
