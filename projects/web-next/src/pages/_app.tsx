96abcb10fa6fdae8ca8b432909e2a04af00cab16
Raul Melo <raulmelo@pop-os.localdomain>
2020-10-11 20:41:18 +0200

    wip: stuffs

--- projects/web-next/src/pages/_app.tsx
+++ projects/web-next/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { ApolloProvider } from '@apollo/client';
-import { AppProps } from 'next/dist/next-server/lib/router/router';
 import { createGlobalStyle } from 'styled-components';
+import App from 'next/app';
+import flat from 'flat';
+import { IntlProvider } from 'react-intl';
 
 import { useApollo } from '@lib/apolloClient';
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import enMessages from '../locales/en.json';
+import ptMessages from '../locales/pt-br.json';
+import { SupportedLanguages } from '@app/types';
 
 const GlobalStyles = createGlobalStyle`
   * {
@@ -11,16 +17,56 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
-const App = ({ Component, pageProps }: AppProps) => {
+const messages = {
+  pt: ptMessages,
+  en: enMessages,
+};
+
+type MyAppProps = AppProps & {
+  language: SupportedLanguages;
+};
+
+const MyApp = ({ Component, pageProps, language }: MyAppProps) => {
   const apolloClient = useApollo(pageProps.initialApolloState);
+
+  console.log(language);
   return (
     <>
       <GlobalStyles />
       <ApolloProvider client={apolloClient}>
-        <Component {...pageProps} />
+        <IntlProvider locale={language} messages={flat(messages[language])}>
+          <Component {...pageProps} />
+        </IntlProvider>
       </ApolloProvider>
     </>
   );
 };
 
-export default App;
+function getRequestedLocales(request: any): SupportedLanguages {
+  const requestedLocales: string | string[] =
+    request?.locale ||
+    (typeof navigator !== 'undefined' && navigator.languages) ||
+    // IE11
+    (typeof navigator !== 'undefined' && (navigator as any).userLanguage) ||
+    (typeof window !== 'undefined' && (window as any).LOCALE) ||
+    'en';
+
+  return requestedLocales as SupportedLanguages;
+}
+
+MyApp.getInitialProps = async (appContext: any) => {
+  console.log(appContext);
+
+  const { req } = appContext;
+
+  const language = getRequestedLocales(req);
+
+  const appProps = await App.getInitialProps(appContext);
+
+  return {
+    ...appProps,
+    language,
+  };
+};
+
+export default MyApp;
