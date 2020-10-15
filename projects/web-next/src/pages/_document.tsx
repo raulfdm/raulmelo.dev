a78f992419c7c43a5c3198b3095310388a8921a0
Raul Melo <melo.raulf@gmail.com>
2020-10-15 14:51:43 +0200

    feat: add theming

--- projects/web-next/src/pages/_document.tsx
+++ projects/web-next/src/pages/_document.tsx
@@ -42,6 +42,37 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="/css/fonts.css" />
         </Head>
         <body>
+          {/* Global Theme handler */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function themeHandler() {
+                window.__onThemeChange = function () {};
+                function setTheme(newTheme) {
+                  window.__theme = newTheme;
+                  preferredTheme = newTheme;
+                  document.body.className = newTheme;
+                  window.__onThemeChange(newTheme);
+                }
+                var preferredTheme;
+                try {
+                  preferredTheme = localStorage.getItem('theme');
+                } catch (err) {}
+                window.__setPreferredTheme = function (newTheme) {
+                  setTheme(newTheme);
+                  try {
+                    localStorage.setItem('theme', newTheme);
+                  } catch (err) {}
+                };
+                var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+                darkQuery.addListener(function (e) {
+                  window.__setPreferredTheme(e.matches ? 'dark' : 'light');
+                });
+                setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
+              })()
+            `,
+            }}
+          />
           <Main />
           <NextScript />
         </body>
