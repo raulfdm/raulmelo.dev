1ddadc922b839ed04c71761c334da22e8930bf6a
Raul Melo <melo.raulf@gmail.com>
2020-10-19 21:54:43 +0200

    refactor: move BlogTheme to Themes folder

--- projects/web-next/src/components/Themes/BlogTheme.tsx
+++ projects/web-next/src/components/Themes/BlogTheme.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import { AppThemeProvider } from '@contexts/AppTheme';
-import { blogGlobalStyles } from './styles/globals';
+import { blogGlobalStyles } from '@screens/Blog/styles/globals';
 import { GlobalStyles } from '@styles/index';
+import { SiteLayout } from '@components/Layouts';
 
 export const BlogTheme: React.FC = ({ children }) => {
   return (
-    <>
+    <AppThemeProvider>
       <GlobalStyles global={blogGlobalStyles} />
-      <AppThemeProvider>{children}</AppThemeProvider>
-    </>
+      <SiteLayout as="main">{children}</SiteLayout>
+    </AppThemeProvider>
   );
 };
