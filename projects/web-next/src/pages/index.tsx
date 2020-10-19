b5180d29e245868f3c25262ed7bacfcdc9b1e9d2
Raul Melo <melo.raulf@gmail.com>
2020-10-19 21:56:16 +0200

    feat: create HomePage component

--- projects/web-next/src/pages/index.tsx
+++ projects/web-next/src/pages/index.tsx
@@ -1,16 +1,7 @@
-import { AppThemeProvider } from '@contexts/AppTheme';
-import { styled } from '@styles/styled';
-
-const Title = styled.h1`
-  font-family: ${({ theme }) => theme.font.contentSerif};
-`;
+import { HomePage } from '@screens/Home/HomePage';
 
 const Home = () => {
-  return (
-    <AppThemeProvider>
-      <Title>Hi</Title>
-    </AppThemeProvider>
-  );
+  return <HomePage />;
 };
 
 export default Home;
